4a2100f852f46ac33cd1dcad6447110cfe4cca47
Chad Armitstead <TheHob@users.noreply.github.com>
2018-02-14 15:49:49 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,6 @@
+//--------------------------------------------------------------------
+// Modules
+module "module" {
+  source  = "atlas.hashicorp.com/example-organization/module/aws"
+  version = "0.0.1"
+}
